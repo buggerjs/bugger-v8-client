82189b7c6e830e155e42519f346d042cd4526a12
Jan Krems <jan.krems@groupon.com>
2013-07-28 21:26:25 +0200

    Yeahi! Test helpers!

--- test/integration/empty.test.js
+++ test/integration/empty.test.js
@@ -1,43 +1,4 @@
-var lastDebugPort = 5858;
-
-function withBugger(name, args) {
-  var execFile = require('child_process').execFile;
-  var path = require('path');
-
-  var createDebugClient = require('../../lib/bugger-v8-client').createDebugClient;
-
-  var rootDir = path.join(__dirname, '..', '..')
-  var filename = path.join(rootDir, 'test', 'buggers', name);
-  var ctx = {
-    debugPort: (++lastDebugPort),
-    child: null,
-    bugger: null
-  };
-
-  if (!Array.isArray(args)) { args = []; }
-  args.unshift(filename);
-  args.unshift('--debug-brk=' + ctx.debugPort);
-
-  beforeEach(function(done) {
-    ctx.child = execFile(process.argv[0], args, { cwd: process.cwd(), env: process.env });
-    ctx.bugger = createDebugClient(ctx.child.pid, lastDebugPort);
-    done();
-  });
-
-  afterEach(function(done) {
-    ctx.child.on('exit', function() {
-      done();
-    });
-    ctx.child.kill();
-
-    // cleanup
-    ctx.child = null;
-    ctx.bugger = null;
-    ctx.debugPort = (++lastDebugPort);
-  });
-
-  return ctx;
-};
+withBugger = require('../helpers/with_bugger');
 
 describe('empty.js', function() {
   var ctx = withBugger('empty.js');
