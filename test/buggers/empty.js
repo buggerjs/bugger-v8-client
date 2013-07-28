9e74114159228a2db5cc54151c3c83152955bdb0
Jan Krems <jan.krems@groupon.com>
2013-07-28 21:11:58 +0200

    Boot up and connect

---
+++ test/buggers/empty.js
@@ -0,0 +1,2 @@
+console.log('foo!');
+console.error('bar!');
---
+++ test/integration/empty.test.js
@@ -0,0 +1,50 @@
+var lastDebugPort = 5858;
+
+function withBugger(name, args) {
+  var execFile = require('child_process').execFile;
+  var path = require('path');
+
+  var createDebugClient = require('../../lib/bugger-v8-client').createDebugClient;
+
+  var rootDir = path.join(__dirname, '..', '..')
+  var filename = path.join(rootDir, 'test', 'buggers', name);
+  var ctx = {
+    debugPort: (++lastDebugPort),
+    child: null,
+    bugger: null
+  };
+
+  if (!Array.isArray(args)) { args = []; }
+  args.unshift(filename);
+  args.unshift('--debug-brk=' + ctx.debugPort);
+
+  beforeEach(function(done) {
+    ctx.child = execFile(process.argv[0], args, { cwd: process.cwd(), env: process.env });
+    ctx.bugger = createDebugClient(ctx.child.pid, lastDebugPort);
+    done();
+  });
+
+  afterEach(function(done) {
+    ctx.child.on('exit', function() {
+      done();
+    });
+    ctx.child.kill();
+
+    // cleanup
+    ctx.child = null;
+    ctx.bugger = null;
+    ctx.debugPort = (++lastDebugPort);
+  });
+
+  return ctx;
+};
+
+describe('empty.js', function() {
+  var ctx = withBugger('empty.js');
+
+  it('boots and connects', function(done) {
+    ctx.bugger.connect(function() {
+      done();
+    });
+  });
+});
